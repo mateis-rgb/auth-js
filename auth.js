0ef570726084dfc2f7de504d94a815a4f07b77be
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-01-16 11:27:07 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,22 @@
 try {
-    const fs = require('fs')
-    const sha1 = require('sha1')
+    const fs = require('fs');
+    const sha1 = require('sha1');
 
-    function addUser() {
-        var name = document.getElementById('name').value
-        var email = document.getElementById('email').value
-        var password = sha1(document.getElementById('password').value)
-        var cpassword = sha1(document.getElementById('cpassword').value)
+    const addUser = () => {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var password = sha1(document.getElementById('password').value);
+        var cpassword = sha1(document.getElementById('cpassword').value);
 
-        if (password == cpassword) {
-            fs.readFile('./ressources/auth/users.json', function (err, data) {
-                if (err) throw err
+        if (password === cpassword) {
+            fs.readFile('./ressources/auth/users.json', (err, data) => {
+                if (err) throw err;
                 else {
-                    const file = JSON.parse(data)
+                    const file = JSON.parse(data);
                     
-                    var result = file.users.filter(usr => {
-                        return usr.password === password && usr.email === email
-                    })
+                    const result = file.users.filter((usr) => {
+                        return usr.password === password && usr.email === email;
+                    });
                     
                     const user_push = {
                         "id": file.users.length + 1,
@@ -25,49 +25,51 @@ try {
                         "password": password
                     }
                     
-                    if (result == user_push) {
-                        console.log('user already exist')
+                    if (result === user_push) {
+                        console.log('user already exist');
                     }
-                    else {
-                        file.users.push(user_push)
                         
-                        const json = JSON.stringify(file, null, "\t")
-                        
-                        fs.writeFile('./ressources/auth/users.json', json, function (err) {
-                            if (err) throw err
-                        })
-                        
-                        console.log('user is added')
-                    }
+                    file.users.push(user_push);
+                    
+                    const json = JSON.stringify(file, null, "\t");
+                    
+                    fs.writeFile('./ressources/auth/users.json', json, (err) => {
+                        if (err) throw err;
+                    });
+                    
+                    console.log('user is added');
+                    
+                    return;
                 }
-            })
+            });
         }
     }
-    function connect() {
-        var email = document.getElementById('email').value
-        var password = sha1(document.getElementById('password').value)
+    
+    const connect = () => {
+        var email = document.getElementById('email').value;
+        var password = sha1(document.getElementById('password').value);
 
-        fs.readFile('./ressources/auth/users.json', function(err, data) {
-            if (err) throw err
-            else {
-                const file = JSON.parse(data)
+        fs.readFile('./ressources/auth/users.json', (err, data) => {
+            if (err) throw err;
+            
+            const file = JSON.parse(data);
 
-                var result = file.users.filter(usr => {
-                    return usr.password === password && usr.email === email
-                })
+            const result = file.users.filter((usr) => {
+                return usr.password === password && usr.email === email;
+            });
 
-                let emptyArray = result.length
+            let emptyArray = result.length();
 
-                if (emptyArray === 0) {
-                    console.log("user not found");
-                }
-                else {
-                    console.log("user conncted");
-                    sessionStorage.setItem('user-state', 'connected')
-                }
-
+            if (emptyArray === 0) {
+                console.log("user not found");
+                return;
             }
-        })
+            
+            console.log("user conncted");
+            sessionStorage.setItem('user-state', 'connected');
+            
+            return;
+        });
     }
 }
 catch (err) {
